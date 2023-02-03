a4f0c0e7f7e4cc9b49caa39d6124676843c6eb84
T-wcs <70718793+T-wcs@users.noreply.github.com>
2023-02-03 11:25:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,20 @@
 
 import os, time, json, smartcard
 from smartcard.System import readers
-from modules import computerLocker, folderCreate, jsonData
+from modules import computerLocker, folderCreate, jsonData, mqttConnect
 
 currentUser = os.environ["USERNAME"]
 folder_path = "C:\\Users\\{}\\AppData\\Local\\Programs\\WinLockerRFID".format(currentUser)
 CardGrantedConfig = "{}\CardGranted.json".format(folder_path)
+OutputJsonLog = "{}\output.json".format(folder_path)
+mqttConfigFile = "{}\mqttConfig.json".format(folder_path)
 
 def init_rfid():
     global card_uid
+    global reader
     # Recherchez tous les lecteurs disponibles
     r = smartcard.System.readers()
-    # Sélectionnez le lecteur WCR330 => la partie NFC/RFID / La partie CARTE A PUCE 0
+    # Sélectionnez le lecteur WCR330 => 1 pour la partie NFC/RFID / La partie CARTE A PUCE 0
     reader = r[1]
     # Connectez-vous au lecteur
     connection = reader.createConnection()
@@ -24,6 +27,19 @@ def init_rfid():
     card_uid = "".join([format(x, "02x") for x in data])
 
 folderCreate.create_folder()
+
+def parse_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        hostname = data['hostname']
+        port = data['Port']
+        login = data['login']
+        password = data['password']
+        topic = data['topic']
+        return hostname, port, login, password, topic
+
+hostname, port, login, password, topic = parse_json(mqttConfigFile)
+
 while True:
     time.sleep(1)
     try:
@@ -31,14 +47,15 @@ while True:
         time.sleep(0.5)
         result = jsonData.check_uid(CardGrantedConfig, card_uid)
         if(result):
-            jsonData.write_to_json_file(card_uid, currentUser)
+            jsonData.write_to_json_file(card_uid, currentUser, str(reader))
+            mqttConnect.send_data_via_mqtt(OutputJsonLog, topic, hostname, port, login, password)
             pass
         else:
-            jsonData.write_to_json_file("Missing", "No Profile")
+            jsonData.write_to_json_file("Missing", "No Profile", str(reader))
             computerLocker.lock_computer()
     except KeyboardInterrupt:
         exit()
     except:
-        jsonData.write_to_json_file("Missing", "No Profile")
+        jsonData.write_to_json_file("Missing", "No Profile", str(reader))
         computerLocker.lock_computer()
         pass
